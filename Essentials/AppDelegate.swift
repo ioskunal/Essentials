8cfa910c86541a988a2df1f4550e008ac2d1aad7
Kunal Gupta <ioskunal@gmail.com>
2020-05-23 23:50:41 +0530

    Minor changes

---
+++ Essentials/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Essentials
+//
+//  Created by Kunal Gupta on 21/05/20.
+//  Copyright © 2020 Kunal Gupta. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
